494fda0b48dde3b7b910c77aee4468bd0fafa062
NSPilipchak <kolich@ua.fm>
2020-11-28 20:16:02 +0200

    - add components

---
+++ src/components/Employees/Employees.js
@@ -0,0 +1,13 @@
+import './Employess.css';
+import EmployessBirthday from "../EmployeesBirthday/EmployeesBirthday.js";
+
+function Employess() {
+    return (
+        <div className="Employess">
+            Employees
+            <EmployessBirthday/>
+        </div>
+    );
+}
+
+export default Employess;
---
+++ src/components/EmployeesBirthday/EmployeesBirthday.js
@@ -0,0 +1,11 @@
+import './EmployessBirthday.css';
+
+function EmployessBirthday() {
+    return (
+        <div className="EmployessBirthday">
+            Employees birthday
+        </div>
+    );
+}
+
+export default EmployessBirthday;
