ae517f960d53f1d6962dec84ed98b9c4c7a366d4
NSPilipchak <kolich@ua.fm>
2020-12-05 14:04:49 +0200

    - class Employess. get all Users and render

--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -1,13 +1,56 @@
 import './Employess.css';
-import EmployessBirthday from "../EmployeesBirthday/EmployeesBirthday.js";
+import React from 'react';
 
-function Employess() {
-    return (
-        <div className="Employess">
-            Employees
-            <EmployessBirthday/>
-        </div>
-    );
+class Employess extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            users: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        users: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: false,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, users} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {users.map(user => (
+                        <li key={user.id}>
+                            {user.lastName} {user.firstName}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
 }
 
 export default Employess;
