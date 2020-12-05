14c12ad177438b75a3885e30bb3582381300873f
NSPilipchak <kolich@ua.fm>
2020-12-05 23:28:38 +0200

    - build by table

--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -1,6 +1,9 @@
 import './Employess.css';
 import React from 'react';
 
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 class Employess extends React.Component {
     constructor(props) {
         super(props);
@@ -40,16 +43,56 @@ class Employess extends React.Component {
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
+            this.sortArray(users)
             return (
-                <ul>
-                    {users.map(user => (
-                        <li key={user.id}>
-                            {user.lastName} {user.firstName}
-                        </li>
-                    ))}
-                </ul>
+                <table>
+                    <thead>
+                    {this.getColName()}
+                    </thead>
+                    {this.getColUser(users)}
+                </table>
+
             );
         }
+    }
+
+    sortArray(users) {
+        users.sort(function (a, b) {
+            var nameA = a.lastName.toLowerCase(), nameB = b.lastName.toLowerCase()
+            if (nameA < nameB)
+                return -1
+            if (nameA > nameB)
+                return 1
+            return 0
+        })
+    }
+
+    getColName() {
+        return (<tr>
+            {alphabet.map(lit => (
+                <th key={lit}>
+                    {lit}
+                </th>
+            ))}
+        </tr>)
+    }
+
+    getColUser(users) {
+        return (
+            <tr>
+                {alphabet.map(lit => (
+                    <th>
+                        <ul>
+                            {users.map(user => {
+                                if (user.lastName.slice(0, 1) === lit) {
+                                    return <li key={user.id} id={user.id}>{user.lastName} {user.firstName}</li>;
+                                }
+                            })}
+                        </ul>
+                    </th>)
+                )}
+            </tr>
+        )
     }
 }
 
