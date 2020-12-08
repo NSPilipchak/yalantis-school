90dd71769777f24b16aa85920b3ea0614e1394bc
NSPilipchak <kolich@ua.fm>
2020-12-08 10:49:18 +0200

    move constant from api to Const.js

---
+++ src/service/Const.js
@@ -0,0 +1,47 @@
+export function getMonth() {
+    return [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+}
+
+export function getAlphabet() {
+    return [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z"
+    ];
+}
